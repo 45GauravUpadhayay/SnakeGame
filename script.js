52c95162ddcc26eeb797b38e720f5e6d764ea3ec
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-14 14:28:33 +0530

    snake running and food consuming is completed

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHight);
+let intervalId = null;
+let food = {
+    x:Math.floor(Math.random()*rows), y:Math.floor(Math.random()*cols)
+}
 
 const blocks = [];
 const snake = [{
@@ -23,14 +27,10 @@ for(let row = 0; row<rows; row++){
 
 
 function render(){
-    snake.forEach((segment) => {
-        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
-    })
-}
-
-setInterval(() => {
-
     let head = null;
+
+    blocks[`${food.x}-${food.y}`].classList.add("food");
+
     if(direction === "left"){
         head = {x:snake[0].x, y:snake[0].y-1}
     }
@@ -44,6 +44,22 @@ setInterval(() => {
         head = {x:snake[0].x-1, y:snake[0].y}
     }
 
+    if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
+        alert("Game Over");
+        clearInterval(intervalId);
+    }
+
+    if(head.x === food.x && head.y === food.y){
+        blocks[`${food.x}-${food.y}`].classList.remove("food");
+        food = {
+            x: Math.floor(Math.random()*rows), 
+            y: Math.floor(Math.random()*cols)
+        }
+        blocks[`${food.x}-${food.y}`].classList.add("food");
+
+        snake.unshift(head);
+    }
+
     snake.forEach((segment) => {
         blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
     })
@@ -51,6 +67,12 @@ setInterval(() => {
     snake.unshift(head);
     snake.pop();
 
+    snake.forEach((segment) => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
+}
+
+intervalId = setInterval(() => {
     render();
 }, 300);
 
