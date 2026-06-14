d120b548270dc4ad4173939a32b33236560ba143
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-14 13:41:44 +0530

    Snake is almost Ready

--- script.js
+++ script.js
@@ -8,12 +8,8 @@ const rows = Math.floor(board.clientHeight / blockHight);
 const blocks = [];
 const snake = [{
     x:1,y:3
-},{
-    x:1,y:4
-},{
-    x:1,y:5
 }]
-let direction = "right";
+let direction = "down";
 
 for(let row = 0; row<rows; row++){
     for(let col = 0; col<cols; col++){
@@ -29,11 +25,46 @@ for(let row = 0; row<rows; row++){
 function render(){
     snake.forEach((segment) => {
         blocks[`${segment.x}-${segment.y}`].classList.add("fill");
-
     })
 }
 
 setInterval(() => {
-    
+
+    let head = null;
+    if(direction === "left"){
+        head = {x:snake[0].x, y:snake[0].y-1}
+    }
+    else if(direction === "right"){
+        head = {x:snake[0].x, y:snake[0].y+1}
+    }
+    else if(direction === "down"){
+    head = {x:snake[0].x+1, y:snake[0].y}
+    }
+    else if(direction === "up"){
+        head = {x:snake[0].x-1, y:snake[0].y}
+    }
+
+    snake.forEach((segment) => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+    })
+
+    snake.unshift(head);
+    snake.pop();
+
     render();
 }, 300);
+
+addEventListener("keydown", (eve) => {
+    if(event.key === "ArrowUp"){
+        direction = "up";
+    }
+    else if(event.key === "ArrowDown"){
+        direction = "down";
+    }
+    else if(event.key === "ArrowRight"){
+        direction = "right";
+    }
+    else {
+        direction = "left"
+    }
+}) 
